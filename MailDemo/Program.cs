f93d7b8da0fee43c1faf4ca1c62a613044fc43e7
Stuart Northcott <imorital@hotmail.com>
2025-08-26 18:17:32 +0100

    Add MailDemo project with email sending functionality
    Updated solution and project files for the new MailDemo
    application targeting .NET 8.0. Implemented email sending
    features using MailKit and MimeKit in Program.cs, including
    simple emails, multi-part emails with attachments, and
    embedded images. Added sample attachment and image files.
    Revised README.md for improved documentation and setup
    instructions.

---
+++ MailDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Utils;
+
+var from = new MailboxAddress("Stuart", "stu@imorital.net");
+var to = new MailboxAddress("Bob", "bob@imorital.net");
+
+// Send a simple text email using MimeKit and MailKit
+
+var message = new MimeMessage();
+message.From.Add(from);
+message.To.Add(to);
+
+message.Subject = "Test email from MimeKit";
+
+message.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+{
+    Text = """
+    This is a test email sent from MimeKit.
+
+    The body here should just be in plain text.
+
+    Let's hope it works!
+    """
+};
+
+using var smtp = new SmtpClient();
+await smtp.ConnectAsync("localhost", 1025);
+await smtp.SendAsync(message);
+await smtp.DisconnectAsync(true);
+
+Console.WriteLine("Simple Mail Sent!");
+
+// Send a multi part email with an attachment
+message = new MimeMessage();
+message.From.Add(from);
+message.To.Add(to);
+message.Subject = "Test email with attachment from MimeKit";
+var bodybuilder = new BodyBuilder();
+bodybuilder.TextBody = "The body here should just be in plain text.";
+bodybuilder.HtmlBody = "<p>The body here should be in <b>HTML</b>.</p>";
+bodybuilder.Attachments.Add("attachment.txt");
+message.Body = bodybuilder.ToMessageBody();
+
+using var secondSmtp = new SmtpClient();
+await secondSmtp.ConnectAsync("localhost", 1025);
+await secondSmtp.SendAsync(message);
+await secondSmtp.DisconnectAsync(true);
+
+Console.WriteLine("Multi-Part Mail Sent!");
+
+// Send an email with an embedded image
+message = new MimeMessage();
+message.From.Add(from);
+message.To.Add(to);
+message.Subject = "Test email with embedded image from MimeKit";
+var builder = new BodyBuilder();
+var image = builder.LinkedResources.Add("image.png");
+image.ContentId = MimeUtils.GenerateMessageId();
+builder.HtmlBody = string.Format("<p>This email contains an embedded image.</p><img src=\"cid:{0}\" alt=\"Image alt text\">", image.ContentId);
+message.Body = builder.ToMessageBody();
+
+using var thirdSmtp = new SmtpClient();
+await thirdSmtp.ConnectAsync("localhost", 1025);
+await thirdSmtp.SendAsync(message);
+await thirdSmtp.DisconnectAsync(true);
+
+Console.WriteLine("Mail with Embedded Image Sent!");
+
+Console.WriteLine("*** Demo Finished ***");
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Edu_EmailDemo
-A small project to demonstrate how to send emails correctly
+# Introduction
+
+This is a small project to demonstrate how to send emails correctly using [MimeKit](https://github.com/jstedfast/MimeKit) 
+and [MailKit](https://github.com/jstedfast/MailKit), based on a [YouTube video](https://youtu.be/1KrqMZNTvpY?si=42k7wbLpMLsTvOLM). 
+I have also document the usage detaiuls on my wiki [here](https://imorital.co.uk/CSharp/Tips/Introduction-to-Sending-Email/), which 
+should be used as the main source of information for what is going on here. This is just the implementation to demonstrate the concept.
+
+The Demo is a simple console application that sends an email using SMTP.
+
+## Build and Test
+
+To build and test the project, you will need to have .NET SDK installed on your machine. You can download it from [here](https://dotnet.microsoft.com/download). The code uses .Net 8.0
+
+To actually send the email we need to use a real SMTP server. You can use any SMTP server you have access to, but for this demo, I will use [Mailpit](https://mailpit.axllent.org).
+
+To install tihs on Docker see [this page](https://hub.docker.com/r/axllent/mailpit), but baiscally I ran the following command:
+
+```bash
+docker run -d --name=mailpit --restart unless-stopped -p 8025:8025 -p 1025:1025 axllent/mailpit
+```
+
+With this running you can access the web interface at [http://localhost:8025](http://localhost:8025) and the SMTP server is running on `localhost:1025`.
+
+Now we can run the project and our email should appear in the Mailpit web interface.
+
